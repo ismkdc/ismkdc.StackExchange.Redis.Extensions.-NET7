1170aea1d58f3995664713548cfa515084a7db4a
İsmail Kundakcı <ism.kundakci@hotmail.com>
2022-11-19 00:56:13 +0300

    nuget done

--- StackExchange.Redis.Extensions.AspNetCore/obj/Release/net7.0/StackExchange.Redis.Extensions.AspNetCore.AssemblyInfo.cs
+++ StackExchange.Redis.Extensions.AspNetCore/obj/Release/net7.0/StackExchange.Redis.Extensions.AspNetCore.AssemblyInfo.cs
@@ -10,7 +10,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("StackExchange.Redis.Extensions.AspNetCore")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("NET7-StackExchange.Redis.Extensions.AspNetCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
